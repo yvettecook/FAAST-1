e0aace400620c24258d0f346a7c34dbf26cbdb79
Sandrine <sandrine.zhangferron@gmail.com>
2014-09-20 17:37:56 +0100

    Added travel and stop methods

--- lib/train.rb
+++ lib/train.rb
@@ -1,11 +1,24 @@
 class Train
 
-def initialize(coaches=1)
-	@coaches = coaches
-end
+	def initialize(coaches=1)
+		@coaches = coaches
+		stop
+	end
 
-def has_coaches?
-	!@coaches.nil?
-end
+	def has_coaches?
+		!@coaches.nil?
+	end
+
+	def at_station?
+		@at_station
+	end
+
+	def travel
+		@at_station = false
+	end
+
+	def stop
+		@at_station = true
+	end
 
 end
--- spec/train_spec.rb
+++ spec/train_spec.rb
@@ -2,9 +2,25 @@ require './lib/train.rb'
 
 describe  Train do
 
+let(:train) {Train.new}
+
 	it "has coaches" do
-		train = Train.new
 		expect(train).to have_coaches
 	end
-	
+
+	it "should be at a station" do
+		expect(train).to be_at_station
+	end
+
+	it "can travel between stations" do
+		train.travel
+		expect(train).not_to be_at_station
+	end
+
+	it "can stop at a station" do
+		train.travel
+		train.stop
+		expect(train).to be_at_station
+	end
+
 end
