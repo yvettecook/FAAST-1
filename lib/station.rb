f5bd1f2e0a1dbfdd455ccaaff4f2cac107403190
Sandrine <sandrine.zhangferron@gmail.com>
2014-09-20 23:49:04 +0100

    Station can allow users enter and exit

--- lib/station.rb
+++ lib/station.rb
@@ -2,6 +2,7 @@ class Station
 
 	def initialize
 		@trains = []
+		@users = []
 	end
 
 	def train_count
@@ -20,6 +21,18 @@ class Station
 	def allow_leave(train)
 		train.travel
 		@trains.delete(train)
+	end
+
+	def user_count
+		@users.count
+	end
+
+	def let_enter(user)
+		@users << user
+	end
+
+	def let_exit(user)
+		@users.delete(user)
 	end
 
 end
