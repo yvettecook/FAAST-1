ad94f3eb9c5d6cee5346290714f9db83a82557ac
Sandrine <sandrine.zhangferron@gmail.com>
2014-09-21 15:56:07 +0100

    Refactored PassengerHolder

--- spec/user_container_spec.rb
+++ spec/user_container_spec.rb
@@ -3,26 +3,25 @@ require './lib/coach.rb'
 require './lib/user_container.rb'
 require './lib/user.rb'
 
-class ContainerHolder; include UserContainer; end
+class PassengerHolder; include UserContainer; end
 
-describe ContainerHolder do
+describe PassengerHolder do
 	
 	let(:station) {Station.new}
 	let(:coach) {Coach.new}
-	let(:space) {ContainerHolder.new}
+	let(:space) {PassengerHolder.new}
+	let(:user) {User.new}
 
 	it "has no users at opening" do
 		expect(space.user_count).to eq(0)
 	end
 
 	it "can allow users to enter" do
-		user = double :user
 		space.let_enter(user)
 		expect(space.user_count).to eq(1)
 	end
 
 	it "can allow users to exit" do
-		user = double :user
 		space.let_enter(user)
 		space.let_exit(user)
 		expect(space.user_count).to eq(0)
@@ -34,7 +33,6 @@ describe ContainerHolder do
 	end
 
 	it "should not allow a new user when it's full" do
-		user = double :user
 		fill(space)
 		expect{space.let_enter(user)}.to raise_error(RuntimeError)
 	end
@@ -42,6 +40,5 @@ describe ContainerHolder do
 	def fill(space)
 		space.capacity.times {space.let_enter(User.new)}
 	end
-
 
 end
