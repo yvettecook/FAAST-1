5eeed8304137ccb855a4750623c9d50aa58d285e
Sandrine <sandrine.zhangferron@gmail.com>
2014-09-21 10:03:59 +0100

    User can touch in to enter station

--- lib/user.rb
+++ lib/user.rb
@@ -1,2 +1,14 @@
 class User
+
+	def initialize
+		@in_station = false
+	end
+
+	def in_station?
+		@in_station
+	end
+
+	def touch_in(station)
+		@in_station = true
+	end
 end
--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -1,4 +1,19 @@
 require './lib/user.rb'
+require './lib/station.rb'
+
 describe User do 
+
+	let(:user) {User.new}
+
+	it "is not in a station" do
+		expect(user).not_to be_in_station
+	end
+
+	it "can be touch in to enter a station" do
+		station = double :station
+		user.touch_in(station)
+		expect(user).to be_in_station
+	end
+
 
 end
